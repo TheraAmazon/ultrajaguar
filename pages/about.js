3928b56447c217c5939843d0a367edba1d468030
Thera <dereksidi@thera.foundation>
2022-07-12 01:39:14 -0400

    more About us!

--- pages/about.js
+++ pages/about.js
@@ -58,6 +58,11 @@ export default function Home() {
                         </div>
 
                         <div className="md:flex md:items-start md:-mx-4">
+                        <span className="inline-block p-2 text-green-500 bg-green-100 rounded-xl md:mx-4 dark:text-white dark:bg-green-500">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+                        </svg>
+                        </span>
 
                             <div className="mt-4 md:mx-4 md:mt-0">
                                 <h1 className="text-2xl font-semibold text-gray-700 capitalize dark:text-white">Fast and Modern</h1>
@@ -70,33 +75,48 @@ export default function Home() {
 
                         <div className="md:flex md:items-start md:-mx-4">
                             <span className="inline-block p-2 text-green-500 bg-green-100 rounded-xl md:mx-4 dark:text-white dark:bg-green-500">
-                                <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4" />
-                                </svg>
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M14.828 14.828a4 4 0 01-5.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                            </svg>
                             </span>
 
                             <div className="mt-4 md:mx-4 md:mt-0">
-                                <h1 className="text-2xl font-semibold text-gray-700 capitalize dark:text-white">Zero Configrations</h1>
+                                <h1 className="text-2xl font-semibold text-gray-700 capitalize dark:text-white">Zero Carbon Footprint</h1>
 
                                 <p className="mt-3 text-gray-500 dark:text-gray-300">
-                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident ab nulla quod dignissimos vel non corrupti doloribus voluptatum eveniet
+                                    Ultra Jaguar uses light weight languages and searchs the web without using big hardware that uses excessive electricity.
                                 </p>
                             </div>
                         </div>
 
                         <div className="md:flex md:items-start md:-mx-4">
                             <span className="inline-block p-2 text-green-500 bg-green-100 rounded-xl md:mx-4 dark:text-white dark:bg-green-500">
-                                <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17.657 18.657A8 8 0 016.343 7.343S7 9 9 10c0-2 .5-5 2.986-7C14 5 16.09 5.777 17.656 7.343A7.975 7.975 0 0120 13a7.975 7.975 0 01-2.343 5.657z" />
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9.879 16.121A3 3 0 1012.015 11L11 14H9c0 .768.293 1.536.879 2.121z" />
-                                </svg>
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M21 15.546c-.523 0-1.046.151-1.5.454a2.704 2.704 0 01-3 0 2.704 2.704 0 00-3 0 2.704 2.704 0 01-3 0 2.704 2.704 0 00-3 0 2.704 2.704 0 01-3 0 2.701 2.701 0 00-1.5-.454M9 6v2m3-2v2m3-2v2M9 3h.01M12 3h.01M15 3h.01M21 21v-7a2 2 0 00-2-2H5a2 2 0 00-2 2v7h18zm-3-9v-2a2 2 0 00-2-2H8a2 2 0 00-2 2v2h12z" />
+                            </svg>
                             </span>
 
                             <div className="mt-4 md:mx-4 md:mt-0">
-                                <h1 className="text-2xl font-semibold text-gray-700 capitalize dark:text-white">elegant Dark Mode</h1>
+                                <h1 className="text-2xl font-semibold text-gray-700 capitalize dark:text-white">Your information, your privacy</h1>
 
                                 <p className="mt-3 text-gray-500 dark:text-gray-300">
-                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident ab nulla quod dignissimos vel non corrupti doloribus voluptatum eveniet
+                                    We don't sell our users data so relax and have Fun searching the web!
+                                </p>
+                            </div>
+                        </div>
+
+                        <div className="md:flex md:items-start md:-mx-4">
+                            <span className="inline-block p-2 text-green-500 bg-green-100 rounded-xl md:mx-4 dark:text-white dark:bg-green-500">
+                           <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                           <path stroke-linecap="round" stroke-linejoin="round" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
+                            </svg>
+                            </span>
+
+                            <div className="mt-4 md:mx-4 md:mt-0">
+                                <h1 className="text-2xl font-semibold text-gray-700 capitalize dark:text-white">Green Incentives</h1>
+
+                                <p className="mt-3 text-gray-500 dark:text-gray-300">
+                                    Ultra Jaguar is partnered with over 8+ Institutes in the Amazon Rainforest, and our donations are directed to incentivize intelligent social moviments that will not only refoest trees but also plant new ideas on the amazon societies. 
                                 </p>
                             </div>
                         </div>
